94ae57c21e0289ba7388c016df6c46bd963786a9
Matthew Lai <mklai10@hotmail.com>
2025-02-12 15:48:25 -0800

    refactored no movement, last commit missed this file

--- noButton.js
+++ noButton.js
@@ -6,15 +6,11 @@ function moveNoButton(event) {
     let currentTop = window.getComputedStyle(noButton,null).getPropertyValue('top').match(/\d+/)[0];
 
     let rect = noButton.getBoundingClientRect();
-    // console.log(rect.x, event.x);
     let diffX = event.x - rect.x;
     let diffY = event.y - rect.y;
 
     let distance = Math.sqrt((diffX * diffX) + (diffY * diffY));
 
-    // console.log(screen.availHeight);
-    // console.log(rect.y);
-    
     // if (Math.abs(diffX) <= 50) {
     //     noButton.style.left = `${parseInt(currentLeft) + (diffX > 0 ? -1 : 1)}px`;
     // }
@@ -24,43 +20,26 @@ function moveNoButton(event) {
     // }
 
     if (distance <= 50) {
-        // noButton.style.left = `${parseInt(currentLeft) + (diffX > 0 ? -(Math.abs(diffX)) : (Math.abs(diffX)))}px`;
-        // noButton.style.top = `${parseInt(currentTop) + (diffY > 0 ? (-1 / diffY) : (1 / diffY))}px`;
-
-        let onWall = false;
         let rightVelocity = 1;
         let leftVelocity = 1;
         let upVelocity = 1;
         let downVelocity = 1;
         
         if (rect.x >= screen.availWidth - 50) {
-            // onWall = true;
-            // onRightWall(event, noButton, currentTop, currentLeft);
             rightVelocity = 0;
         } 
         
         if (rect.x <= 50) {
-            // onWall = true;
-            // onLeftWall(event, noButton, currentTop, currentLeft);
             leftVelocity = 0;
         } 
         
         if (rect.y >= screen.availHeight - 120) {
-            // onWall = true;
-            // onBottomWall(event, noButton, currentTop, currentLeft);
             downVelocity = 0;
         }  
         
         if (rect.y <= 50) {
-            // onWall = true;
-            // onTopWall(event, noButton, currentTop, currentLeft);
             upVelocity = 0;
         } 
-        
-        // if (!onWall) {
-        //     noButton.style.left = `${parseInt(currentLeft) - (diffX / 5)}px`;
-        //     noButton.style.top = `${parseInt(currentTop) - (diffY / 5)}px`;
-        // }
 
         console.log(rightVelocity);
 
@@ -68,49 +47,3 @@ function moveNoButton(event) {
         noButton.style.top = `${parseInt(currentTop) - (diffY > 0 ? (diffY * upVelocity / 5) : (diffY * downVelocity / 5))}px`;
     }
 }
-
-// function onRightWall(event, button, top, left) {
-//     let rect = button.getBoundingClientRect();
-//     let diffX = event.x - rect.x;
-//     let diffY = event.y - rect.y;
-
-//     console.log(diffX)
-//     button.style.top = `${parseInt(top) - (diffY / 5)}px`;
-//     if (diffX > 0) {
-//         button.style.left = `${parseInt(left) - (diffX / 5)}px`;
-//     }
-// }
-
-// function onLeftWall(event, button, top, left) {
-//     let rect = button.getBoundingClientRect();
-//     let diffX = event.x - rect.x;
-//     let diffY = event.y - rect.y;
-
-//     button.style.top = `${parseInt(top) - (diffY / 5)}px`;
-//     if (diffX < 0) {
-//         button.style.left = `${parseInt(left) - (diffX / 5)}px`;
-//     }
-// }
-
-// function onTopWall(event, button, top, left) {
-//     let rect = button.getBoundingClientRect();
-//     let diffX = event.x - rect.x;
-//     let diffY = event.y - rect.y;
-
-//     button.style.left = `${parseInt(left) - (diffX / 5)}px`;
-//     if (diffY < 0) {
-//         button.style.top = `${parseInt(top) - (diffY / 5)}px`;
-//     }
-// }
-
-// function onBottomWall(event, button, top, left) {
-//     let rect = button.getBoundingClientRect();
-//     let diffX = event.x - rect.x;
-//     let diffY = event.y - rect.y;
-
-//     button.style.left = `${parseInt(left) - (diffX / 5)}px`;
-//     if (diffY > 0) {
-//         button.style.top = `${parseInt(top) - (diffY / 5)}px`;
-//     }
-// }
-
